d51e86e065127f8286526d37dd83bc7f8bff0f28
Victor2003-dotcom <victoralejandro774@gmail.com>
2025-07-11 01:22:11 -0600

    tareaa.py

--- holamundo.py
+++ holamundo.py
@@ -21,11 +21,3 @@ print(f"Punto 1: {punto1}")
 print(f"Punto 2: {punto2}")
 print(f"Distancia Euclidiana: {euclidiana:.2f}")
 print(f"Distancia Manhattan: {manhattan}")
-
-echo "# tarea" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Victor2003-dotcom/tarea.git
-git push -u origin main
